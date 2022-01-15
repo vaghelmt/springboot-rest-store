b67a6e76fea739eba553539a800a8ed3ded5a73b
Mitul Vaghela <mitul.vaghela@gmail.com>
2022-01-15 16:17:34 -0500

    Initial set of spring boot project

---
+++ src/main/java/com/vaghelmt/store/StoreApplication.java
@@ -0,0 +1,15 @@
+package com.vaghelmt.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
